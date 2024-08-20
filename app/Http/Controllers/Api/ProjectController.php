8aef533989670da96ba1a39380fb03726601cc86
SimoneColombo1 <simcolombo2@gmail.com>
2024-08-20 22:39:08 +0200

    Aggiunta la possibilità di restiruire un json  con all'interno i progetti

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\projects;
 
 class ProjectController extends Controller
 {
-    //
+
+
+
+    public function index()
+    {
+
+        $project = projects::paginate(20);
+        return response()->json(
+
+            [
+                'success' => true,
+                'results' => $project
+            ]
+
+        );
+    }
+
+
+    public function Show(projects $project) {}
 }
